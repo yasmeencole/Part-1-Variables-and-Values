43b69562599411d4d91d925dc0a29e2d7a4adcef
Yasmeen Cole <yasmeencole1@gmail.com>
2020-10-31 12:27:41 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+   This is a code comment block. What you type here will be colored green and 
+   won't be run by JavaScript. It's used to store messages about your code.
+
+   WRITE ALL OF YOUR CODE HERE, NOT IN THE BLACK CONSOLE SECTION BELOW.
+*/
+let januaryBill = 43.11
+let februaryBill = 135.76
+let marchBill = 164.92
+let aprilBill = 167.36
+let mayBill = 198.69
+let juneBill = 203.55
+let julyBill = 250.98
+let augustBill = 300.78
+let septemberBill = 287.45
+let octoberBill = 201.98
+let novemberBill = 143.04
+let decemberBill = 145.32
+
+const total = januaryBill + februaryBill + marchBill + aprilBill + mayBill + juneBill + julyBill + augustBill + septemberBill + octoberBill + novemberBill + decemberBill
+console.log(total)
